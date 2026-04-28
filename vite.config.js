5cbc15817f8f7cb948d8ecb830f1de530e604b42
0x0desha74 <mudesha74@gmail.com>
2026-04-28 17:56:56 +0300

    upload project logic

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          clerk: ['@clerk/clerk-react'],
+          docx: ['docx', 'file-saver'],
+          mammoth: ['mammoth'],
+          dnd: ['@dnd-kit/core', '@dnd-kit/sortable', '@dnd-kit/utilities']
+        }
+      }
+    }
+  }
 })
